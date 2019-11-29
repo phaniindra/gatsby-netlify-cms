fdc1e65fd0aab4388ff60cc89fae2b3fb5255146
paeruva+1-at-154091047068 <paeruva+1-at-154091047068>
2019-11-29 10:00:51 +0530

    added custom page in nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,6 +63,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
+              <Link className="navbar-item" to="/custom">
+                Custom
+              </Link>
               <Link className="navbar-item" to="/products">
                 Products
               </Link>
