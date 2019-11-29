cbfbae05928931c17dafa8122a4e9f4fbc6f9047
paeruva+1-at-154091047068 <paeruva+1-at-154091047068>
2019-11-29 14:32:44 +0530

    removed un used ones

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,24 +60,27 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
+              <Link className="navbar-item">
                 About
               </Link>
-              <Link className="navbar-item" to="/custom">
+              {/* <Link className="navbar-item" to="/custom">
                 Custom
-              </Link>
-              <Link className="navbar-item" to="/products">
+              </Link> */}
+              <Link className="navbar-item">
                 Products
               </Link>
-              <Link className="navbar-item" to="/blog">
+              <Link className="navbar-item" to="/case-study">
+                Case Studies
+              </Link>
+              {/* <Link className="navbar-item" to="/blog">
                 Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
+              </Link> */}
+              {/* <Link className="navbar-item" to="/contact">
                 Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
+              </Link> */}
+              {/* <Link className="navbar-item" to="/contact/examples">
                 Form Examples
-              </Link>
+              </Link> */}
             </div>
             <div className="navbar-end has-text-centered">
               {/* <a
