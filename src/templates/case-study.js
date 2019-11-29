77f51d9e0146c31cb1764720b26f451f298f5ab8
paeruva+1-at-154091047068 <paeruva+1-at-154091047068>
2019-11-29 15:15:59 +0530

    added case study

--- src/templates/case-study.js
+++ src/templates/case-study.js
@@ -17,32 +17,52 @@ export const CaseStudyPostTemplate = ({
     const PostContent = contentComponent || Content
 
     return (
-        <section className="section">
-            {helmet || ''}
-            <div className="container content">
-                <div className="columns">
-                    <div className="column is-10 is-offset-1">
-                        <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-                            {title}
-                        </h1>
-                        <p>{description}</p>
-                        <PostContent content={content} />
-                        {tags && tags.length ? (
-                            <div style={{ marginTop: `4rem` }}>
-                                <h4>Tags</h4>
-                                <ul className="taglist">
-                                    {tags.map(tag => (
-                                        <li key={tag + `tag`}>
-                                            <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        ) : null}
+        <React.Fragment>
+            {/* <div
+                className="full-width-image-container margin-top-0"
+                style={{
+                    backgroundImage: `url('/img/blog-index.jpg')`,
+                }}
+            >
+                <h1
+                        className="has-text-weight-bold is-size-1"
+                        style={{
+                            boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+                            backgroundColor: '#f40',
+                            color: 'white',
+                            padding: '1rem',
+                        }}
+                    >
+                        Latest Stories
+          </h1>
+            </div> */}
+            <section className="section">
+                {helmet || ''}
+                <div className="container content">
+                    <div className="columns">
+                        <div className="column is-10 is-offset-1">
+                            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+                                {title}
+                            </h1>
+                            <p>{description}</p>
+                            <PostContent content={content} />
+                            {tags && tags.length ? (
+                                <div style={{ marginTop: `4rem` }}>
+                                    <h4>Tags</h4>
+                                    <ul className="taglist">
+                                        {tags.map(tag => (
+                                            <li key={tag + `tag`}>
+                                                <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            ) : null}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </React.Fragment>
     )
 }
 
